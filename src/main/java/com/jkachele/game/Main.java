96d3b1257a44edde4f2211ea35af184bbd3d3e37
jkachele <justin@kachele.com>
2023-06-30 14:25:59 -0400

    Added Build System
    Added Gradle build system with LWJGL libraries. Tested build with hello world.

---
+++ src/main/java/com/jkachele/game/Main.java
@@ -0,0 +1,13 @@
+package com.jkachele.game;
+/*
+ * @author      Justin Kachele
+ * @file        Main
+ * @created     Friday Jun 30, 2023 14:23:03 EDT
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
